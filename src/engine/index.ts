ac4871378567b0e0dc47954e5d6823eaa51d72c2
aadithya-vimal <aadithyavimal.work@gmail.com>
2026-08-29 09:07:44 +0530

    feat(engine): export unified core engine interface

---
+++ src/engine/index.ts
@@ -0,0 +1,7 @@
+export * from "./types";
+export * from "./semantic";
+export * from "./validation";
+export * from "./sections";
+export * from "./serialization";
+export * from "./generate";
+export { DOCUMENT_DEFINITIONS, CATEGORY_LABELS, CATEGORIES, getDefinition, getDefinitionBySlug, definitionsByCategory } from "./definitions/catalog";
